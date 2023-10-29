0c0746731765173fd609ae22cd3d713bf56069a9
Angelo <gangelos630@gmail.com>
2023-10-29 12:21:36 -0500

    intento de subida n1

---
+++ ProyNeptunoQuery/Models/Productos.cs
@@ -0,0 +1,9 @@
+﻿namespace ProyNeptunoQuery.Models
+{
+    public class Productos
+    {
+        public int IdProducto {  get; set; }
+        public String NombreProducto { get; set; }
+        public decimal PrecioUnida {  get; set; }
+    }
+}
